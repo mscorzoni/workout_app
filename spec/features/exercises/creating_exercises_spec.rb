ffcab8abb14763b22033a29f1ca626ad477a7a04
Marcos Scorzoni <mlessa@gmail.com>
2018-08-17 16:02:21 +0100

    implemement create and show views for exercises

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -1,25 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Creating Exercise' do
+RSpec.feature "Creating Exercise" do
   before do
-    @john = User.create!(email: 'john@email.com', password: 'password')
+    @john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
     login_as(@john)
+    
+    visit "/"
+
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
   end
-
-  scenario 'with valid inputs' do
-    visit '/'
-    click_link 'My Lounge'
-    click_link 'My Lounge'
-    expect(page).to have_link('Back')
-
-    fill_in 'Duration', with: 70
-    fill_in 'Workout Details', with: "Weight Lifting"
-    fill_in 'Activity date', with: '2018-06-26'
-    click_button 'Create Exercise'
-
-    expect(page).to have_content('Exercise has been created')
+  
+  scenario "with valid inputs" do
+    fill_in "Duration", with: 70
+    fill_in "Workout details", with: "Weight lifting"
+    fill_in "Activity date", with: 3.days.ago
+    click_button "Create Exercise"
+    
+    expect(page).to have_content("Exercise has been created")
+    
     exercise = Exercise.last
     expect(current_path).to eq(user_exercise_path(@john, exercise))
     expect(exercise.user_id).to eq(@john.id)
   end
+
 end
